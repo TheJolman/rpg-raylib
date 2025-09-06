d1119d135c1d9a0d726bae49cdab8e2f5e8712bf
Josh Holman <josh@jolman.me>
2025-09-05 19:56:51 -0700

    fix nix build

--- nix/package.nix
+++ nix/package.nix
@@ -1,30 +1,27 @@
 {
-  stdenv,
   cmake,
   ninja,
   raylib,
   llvmPackages_20,
-}: let
-  myStdenv = stdenv.override {
-    cc = llvmPackages_20.clang;
-  };
-in
-  myStdenv.mkDerivation {
-    name = "rpg-raylib";
-    version = "0.1.0";
-    src = ../.;
+  git,
+}:
+llvmPackages_20.stdenv.mkDerivation {
+  name = "rpg-raylib";
+  version = "0.1.0";
+  src = ../.;
 
-    nativeBuildInputs = [
-      cmake
-      ninja
-    ];
+  nativeBuildInputs = [
+    cmake
+    ninja
+    git
+  ];
 
-    buildInputs = [
-      raylib
-    ];
+  buildInputs = [
+    raylib
+  ];
 
-    cmakeFlags = [
-      "-DCMAKE_BUILD_TYPE=Release"
-      "-G Ninja"
-    ];
-  }
+  cmakeFlags = [
+    "-DCMAKE_BUILD_TYPE=Release"
+    "-G Ninja"
+  ];
+}
